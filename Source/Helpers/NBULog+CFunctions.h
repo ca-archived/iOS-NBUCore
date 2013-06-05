6f190974eb8b66b49620ba7af040e6fe15b51ad0
利辺羅エルネスト <rivera_ernesto@cyberagent.co.jp>
2013-06-05 16:22:25 +0900

    Fix NBULogCTrace

--- Source/Helpers/NBULog+CFunctions.h
+++ Source/Helpers/NBULog+CFunctions.h
@@ -23,5 +23,5 @@
 #define NBULogCWarn(frmt, ...)   LOG_C_MAYBE(LOG_ASYNC_WARN,    APP_LOG_LEVEL, LOG_FLAG_WARN,     APP_LOG_CONTEXT + APP_MODULE, frmt, ##__VA_ARGS__)
 #define NBULogCInfo(frmt, ...)   LOG_C_MAYBE(LOG_ASYNC_INFO,    APP_LOG_LEVEL, LOG_FLAG_INFO,     APP_LOG_CONTEXT + APP_MODULE, frmt, ##__VA_ARGS__)
 #define NBULogCVerbose(frmt, ...)LOG_C_MAYBE(LOG_ASYNC_VERBOSE, APP_LOG_LEVEL, LOG_FLAG_VERBOSE,  APP_LOG_CONTEXT + APP_MODULE, frmt, ##__VA_ARGS__)
-#define NBULogCTrace()           NBULogCVerbose(@"[%p] %@", self, THIS_METHOD)
+#define NBULogCTrace()           NBULogCVerbose(@"%s", __FUNCTION__)
 
